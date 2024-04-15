510308d57db0071b447a9a659cb9079ce48eaa90
hamza-25 <me677499@gmail.com>
2024-04-15 17:53:48 +0100

    update require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,13 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """check auth require
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """check authorization request
