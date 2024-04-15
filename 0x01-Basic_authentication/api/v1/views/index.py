381c34984b695e4f6020cf109d0700d3bdb807bb
hamza-25 <me677499@gmail.com>
2024-04-15 17:02:22 +0100

    delete prefix

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> str:
     """GET /api/v1/unauthorized
     Return 401
