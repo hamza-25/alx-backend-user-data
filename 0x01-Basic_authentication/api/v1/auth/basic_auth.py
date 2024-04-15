284570594495dd41d867ca5412eb785edf4a7e4a
hamza-25 <me677499@gmail.com>
2024-04-15 23:44:57 +0100

    add method that check valid base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Define BasicAuth Module
 """
 from .auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,16 @@ class BasicAuth(Auth):
         if authorization_header.startswith('Basic '):
             return authorization_header.split(' ')[1]
         return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string
+        """
+        if (base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            b64decode(base64_authorization_header)
+        except Exception:
+            return None
+        return b64decode(base64_authorization_header).decode('utf-8')
