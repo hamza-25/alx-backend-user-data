93e0628cd26bd751db8612b3996ea771d07c96b1
hamza-25 <me677499@gmail.com>
2024-04-23 20:45:19 +0100

    add sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Define app Flask
 """
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 
 
 app = Flask(__name__)
@@ -22,13 +22,26 @@ def users():
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    # print(email)
-    # print(password)
     try:
         user = AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"})
     return jsonify({"email": user.email, "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """login function that check email and password
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    login = AUTH.valid_login(email, password)
+    if not login:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
