f2f7515bff11155e8c7060cf5abf117937bab17c
hamza-25 <me677499@gmail.com>
2024-04-23 22:32:23 +0100

    up

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -68,7 +68,7 @@ def profile() -> str:
     return jsonify({"email": user.email})
 
 
-@app.route('/reset_password', method=['POST'], strict_slashes=False)
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token() -> str:
     """Get reset password token
     """
